f90fe164457dce3c7de868e073367fab499bf7d7
Noman Ali <noman710@protonmail.com>
2022-01-02 15:57:48 -0500

    Solved problem 5

---
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -0,0 +1,11 @@
+function countOfAllBooleans(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] == true || arr[i] == false){
+      count++;
+    }
+  }
+console.log(count);
+}
+// Do not edit this line;
+module.exports = countOfAllBooleans;
