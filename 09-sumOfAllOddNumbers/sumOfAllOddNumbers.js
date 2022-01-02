eda47570d4d05a4c1b97b4413cb02b26afbef639
Noman Ali <noman710@protonmail.com>
2022-01-02 16:38:39 -0500

    Solved problem 10

---
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -0,0 +1,12 @@
+function sumOfAllOddNumbers(nums) {
+  let count = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 1){
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+// Do not edit this line;
+module.exports = sumOfAllOddNumbers;
