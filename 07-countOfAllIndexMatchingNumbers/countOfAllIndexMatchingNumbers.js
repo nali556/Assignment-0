b23912286ee3b1c8c781ca55e83b56a16f89a476
Noman Ali <noman710@protonmail.com>
2022-01-02 16:32:12 -0500

    Solved problem 8

---
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -0,0 +1,14 @@
+function countOfAllIndexMatchingNumbers(nums) {
+  let count = 0;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] == i){
+      count++
+    }
+  }
+  console.log(count);
+}
+/*Note : The markdown file says countOfAllIndexMatchingNumbers([0,2,3,4,4,5,77]) would output 2, put is it not 3 values
+with the same index?
+*/
+// Do not edit this line;
+module.exports = countOfAllIndexMatchingNumbers;
