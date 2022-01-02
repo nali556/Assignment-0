f941923023fa9c251dbc390cc9b325dadecc0cf2
Noman Ali <noman710@protonmail.com>
2022-01-02 18:19:17 -0500

    Solved problem 2

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -3,8 +3,8 @@ function titleCaseEdit(title) {
   for(let i = 0; i < words.length; i++){
     words[i] = words[i][0].toUpperCase() + words[i].substr(1);
   }
-  str = words.join(" ");
-  return str;
+  s = words.join(" ");
+  return s;
 }
 // Do not edit this line;
 module.exports = titleCaseEdit;
